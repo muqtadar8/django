9242b4f603368a02d26bd841930cc6986a196e79
muqtadar8 <muqtadar8@gamil.com>
2023-09-27 11:45:45 +0530

    Static and Dynamic Webpages, Get and Post Mehtods, Added two numbers: form in index.html and function in the demo.views and path in the demo.urls

--- project0/demo/views.py
+++ project0/demo/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 
 def home(request):
 
-    return HttpResponse("Hello world!")
+    return render(request,'index.html')
+
+def add(request):
+
+    val1 = request.POST['num1']
+
+    val2 = request.POST['num2']
+
+    res = int(val1)+int(val2)
+
+    return render(request,'result.html',{'result':res})
